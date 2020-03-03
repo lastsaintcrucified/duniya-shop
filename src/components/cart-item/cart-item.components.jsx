1601b4bd28471c958f7d06a926e2859ca9200b3e
unknown <lastsaintcrucified@gmail.com>
2020-03-03 07:11:31 +0600

    cart-item added

--- src/components/cart-item/cart-item.components.jsx
+++ src/components/cart-item/cart-item.components.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./cart-item.styles.scss";
 
 const cartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div>
-    <img src={imageUrl} />
-    <div>
-      <span>{name}</span>
-      <span>
+  <div className="cart-item">
+    <img src={imageUrl} alt="collection" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
         {quantity} x {price}
       </span>
     </div>
