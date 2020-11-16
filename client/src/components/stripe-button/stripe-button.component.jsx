073ab45c5da751b75e3a526d203e2f07aa378064
unknown <lastsaintcrucified@gmail.com>
2020-11-16 09:20:50 +0600

    front-end moved to client folder and backend is created to handle stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price*100;
     const publishableKey = "pk_test_51HlOqfIUFOPPi1GDDj4XM6WgKY7Cj3HSYHEPdcM9U8IXw2BkSiwt7Mk3oGFuHrJS3wVVkwJ3MvmBDd2X4qxrhRCn00LQEwH7a4";
 
     const onToken = token =>{
-        console.log(token);
-        alert("Payment Successful")
+        axios({
+            url:'payment',
+            method:'post',
+            data:{
+                amount:priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment is successful!!')
+        }).catch(error => {
+            console.log('paymanet error:',JSON.parse(error));
+            alert('There was an issue with your payment, please make sure that you use the provided credit card')
+        })
     }
 
     return (
